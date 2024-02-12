2ae0b294fea1204a1059c489d054a1a8108b3947
Igor Rayak <igor@autokitteh.com>
2024-02-12 16:47:37 +0200

    convert to known return types

--- pkg/query/signature.go
+++ pkg/query/signature.go
@@ -139,6 +139,7 @@ func ExtractSignature(doc DocumentContent, n *sitter.Node) Signature {
 	if rtNode := n.ChildByFieldName(FieldReturnType); rtNode != nil {
 		returnType = doc.Content(rtNode)
 	}
+	_, returnType = StrToSymbolKindAndType(returnType)
 
 	return Signature{
 		Name:       fnName,
