25fdee30b0d570f721d7692aae523e82f095d9ca
Igor Rayak <igor@autokitteh.com>
2024-02-15 11:23:29 +0200

    feat: update autokitteh integrations

--- pkg/autokitteh/builtins/@http.py
+++ pkg/autokitteh/builtins/@http.py
@@ -1,166 +1,42 @@
-# classes are not working meanwhile
-#class HttpArgs():
-#    url: str
-#    params: str|None
-#    headers: str|None
-#    body: str|None
-#    form_data: str|None
-#    content_type: str|None
-#    json_body: str|None
-#    basic_auth: str|None
-#    oauth2: str|None
+def delete(url, params, headers, body, form_body, form_encoding, json_body, auth):
+    """HTTP Delete method request
+    API:
+      https://datatracker.ietf.org/doc/html/rfc9110#name-delete"""
+    pass
 
-def get(args: dict):
+def get(url, params, headers, body, form_body, form_encoding, json_body, auth):
     """HTTP Get method request
-
-    Args:
-      url: str
-      params: Optional[str]
-      headers: Optional[str]
-      body: Optional[str]
-      form_data: Optional[str]
-      content_type: Optional[str]
-      json_body: Optional[str]
-      basic_auth: Optional[str]
-      oauth2: Optional[str]
-
-
-    Returns:
-
-
     API:
       https://datatracker.ietf.org/doc/html/rfc9110#name-get
     """
     pass
 
-def head(args: dict):
+def head(url, params, headers, body, form_body, form_encoding, json_body, auth):
     """HTTP Head method request
-
-    Args:
-      url: str
-      params: Optional[str]
-      headers: Optional[str]
-      body: Optional[str]
-      form_data: Optional[str]
-      content_type: Optional[str]
-      json_body: Optional[str]
-      basic_auth: Optional[str]
-      oauth2: Optional[str]
-
-
-    Returns:
-
-
     API:
       https://datatracker.ietf.org/doc/html/rfc9110#name-head"""
     pass
 
-def post(args: dict):
-    """HTTP Post method request
-
-    Args:
-      url: str
-      params: Optional[str]
-      headers: Optional[str]
-      body: Optional[str]
-      form_data: Optional[str]
-      content_type: Optional[str]
-      json_body: Optional[str]
-      basic_auth: Optional[str]
-      oauth2: Optional[str]
-
-
-    Returns:
-
-
-    API:
-      https://datatracker.ietf.org/doc/html/rfc9110#name-post"""
-    pass
-
-def put(args: dict):
-    """HTTP Put method request
-
-    Args:
-      url: str
-      params: Optional[str]
-      headers: Optional[str]
-      body: Optional[str]
-      form_data: Optional[str]
-      content_type: Optional[str]
-      json_body: Optional[str]
-      basic_auth: Optional[str]
-      oauth2: Optional[str]
-
-
-    Returns:
-
-
-    API:
-      https://datatracker.ietf.org/doc/html/rfc9110#name-put"""
-    pass
-
-def delete(args: dict):
-    """HTTP Delete method request
-
-    Args:
-      url: str
-      params: Optional[str]
-      headers: Optional[str]
-      body: Optional[str]
-      form_data: Optional[str]
-      content_type: Optional[str]
-      json_body: Optional[str]
-      basic_auth: Optional[str]
-      oauth2: Optional[str]
-
-
-    Returns:
-
-
-    API:
-      https://datatracker.ietf.org/doc/html/rfc9110#name-delete"""
-    pass
-
-def options(args: dict):
+def options(url, params, headers, body, form_body, form_encoding, json_body, auth):
     """HTTP Options method request
-
-    Args:
-      url: str
-      params: Optional[str]
-      headers: Optional[str]
-      body: Optional[str]
-      form_data: Optional[str]
-      content_type: Optional[str]
-      json_body: Optional[str]
-      basic_auth: Optional[str]
-      oauth2: Optional[str]
-
-
-    Returns:
-
-
     API:
       https://datatracker.ietf.org/doc/html/rfc9110#name-options"""
     pass
 
-def patch(args:dict):
+def patch(url, params, headers, body, form_body, form_encoding, json_body, auth):
     """HTTP Patch method request
-
-    Args:
-      url: str
-      params: Optional[str]
-      headers: Optional[str]
-      body: Optional[str]
-      form_data: Optional[str]
-      content_type: Optional[str]
-      json_body: Optional[str]
-      basic_auth: Optional[str]
-      oauth2: Optional[str]
-
-
-    Returns:
-
-
     API:
       https://datatracker.ietf.org/doc/html/rfc5789"""
+    pass
+
+def post(args: dict):
+    """HTTP Post method request
+    API:
+      https://datatracker.ietf.org/doc/html/rfc9110#name-post"""
+    pass
+
+def put(url, params, headers, body, form_body, form_encoding, json_body, auth):
+    """HTTP Put method request
+    API:
+      https://datatracker.ietf.org/doc/html/rfc9110#name-put"""
     pass
--- pkg/autokitteh/builtins/@twilio.py
+++ pkg/autokitteh/builtins/@twilio.py
@@ -1,3 +1,12 @@
+"""Twilio.
+
+Twilio is a programmable phone-based communication platform: sending an receiving messages, making and receiving voice calls, and more.
+
+References:
+  - Messaging API overview: https://www.twilio.com/docs/messaging/api
+  - Voice API overview: https://www.twilio.com/docs/voice/api
+"""
+
 def create_message(to: str, from_number: str|None, messaging_service_sid: str|None, body: str|None, media_url: str|None, content_sid: str|None):
     """https://www.twilio.com/docs/messaging/api/message-resource#create-a-message-resource""",
     pass
