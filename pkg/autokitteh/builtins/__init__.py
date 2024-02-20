35d8763b12c763daba79bd118b6bbc2f43c9f14d
Igor Rayak <49944566+ig-ra@users.noreply.github.com>
2024-02-20 18:00:54 +0200

    feat: add AK bootstrap functions (#14)

---
+++ pkg/autokitteh/builtins/__init__.py
@@ -0,0 +1,28 @@
+"Bootstrap - contains common definition that are used in user code."
+
+def nop():
+    pass
+
+def poll(fn: int, pollerfn: callable):
+    pass
+
+def fake(*args, **kwargs):
+    pass
+
+def sleep(*args, **kwargs):
+    pass
+
+def start(*args, **kwargs):
+    pass
+
+def subscribe(*args, **kwargs):
+    pass
+
+def next_event(*args, **kwargs):
+    pass
+
+def unsubscribe(*args, **kwargs):
+    pass
+
+def test():
+    pass
